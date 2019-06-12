01853cb58e1d54b0b8b96b9c841614b13dcc60c3
Nathan Duggan <nathan.duggan0@gmail.com>
2019-06-12 20:56:35 +0000

    Setting up files

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -79,6 +80,10 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+}
+
+DATABASES = {
+    'default': dj_database_url.parse("postgres://evpbsacxfrjjxt:2add01f793a4136d2f3424b203decbde18c0e8a65e9c2bc3cd21866345f8b03d@ec2-79-125-4-72.eu-west-1.compute.amazonaws.com:5432/d3uoc5alvqbphb")
 }
 
 
